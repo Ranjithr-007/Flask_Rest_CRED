75c19e11b723a5c5417bbe7f8ef3b1acb6d34609
Ranjith <ranjithranjukpm333@gmail.com>
2021-12-08 22:27:12 +0530

    CRED operation completed

--- app.py
+++ app.py
@@ -54,7 +54,48 @@ def create_task():
   db.session.commit()
 
   return task_schema.jsonify(new_task)
-    
+
+#Get All Task
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+  all_tasks = Task.query.all()
+  result = tasks_schema.dump(all_tasks)
+  return jsonify(result)
+
+#Get Single Task
+@app.route('/tasks/<id>', methods=['GET'])
+def get_task(id):
+  task = Task.query.get(id)
+  return task_schema.jsonify(task)
+
+#Update a Task
+@app.route('/tasks/<id>', methods=['PUT'])
+def update_task(id):
+  task = Task.query.get(id)
+
+  title = request.json['title']
+  description = request.json['description']
+
+  task.title = title
+  task.description = description
+
+  db.session.commit()
+
+  return task_schema.jsonify(task)
+
+
+#Delete Task
+@app.route('/tasks/<id>', methods=['DELETE'])
+def delete_task(id):
+  task = Task.query.get(id)
+  db.session.delete(task)
+  db.session.commit()
+  return task_schema.jsonify(task)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Welcome to my API'})
 
 
 #Run Server
